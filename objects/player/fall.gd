0e9089614ca385a932dc291d20629530fa02ccfa
Daniel Barry <barrydan9@gmail.com>
2026-01-19 21:38:31 -0500

    various movement updates
    - Updates wind resistance at player object level
    - Implements fall state
    - Fixes bug where you float off the edge of platform
    
    These changes have been tested on an NVIDIA GeForce GTX 1660 Ti.

--- objects/player/fall.gd
+++ objects/player/fall.gd
@@ -1,5 +1,6 @@
 extends PlayerState
 
+
 func update(_delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -9,18 +10,13 @@ func update(_delta):
 		player.velocity.x += player.wind_resistance
 	else:
 		player.velocity.x = move_toward(player.velocity.x, 0, player.SPEED)
-	
-	if Input.is_action_just_pressed("jump") and player.is_on_floor():
-		change_state.emit("jump")
-	
-	if player.velocity.x == 0:
-		change_state.emit("idle")
-	
-	if not player.is_on_floor():
-		change_state.emit("fall")
-	
-	pass
+		player.velocity.x = player.wind_resistance
+		
+	if player.is_on_floor():
+		change_state.emit("walk")
+		
+	# Apply gravity.
+	player.velocity.y += player.get_gravity().y * _delta
 
 func enter():
-	player.Anim.play("walk")
-	pass
+	player.Anim.play("jump")
--- objects/player/idle.gd
+++ objects/player/idle.gd
@@ -3,10 +3,15 @@ extends PlayerState
 func update(_delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	player.velocity.x = player.wind_resistance
+
 	if direction:
 		change_state.emit("walk")
 	if Input.is_action_just_pressed("jump") and player.is_on_floor():
 		change_state.emit("jump")
+
+	# This case is possible because once the wind hits us, we can be moved off an edge.
+	if not player.is_on_floor():
+		change_state.emit("fall")
 
 func enter():
 	player.Anim.play("idle")
